e31e77c2877ce17e77dc402682efabc37396612a
adler-pelzer-dev <adleroee@gmail.com>
2025-06-18 15:43:54 -0300

    update

--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -1,16 +1,23 @@
 import { Component, Signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LucideAngularModule, PanelLeft } from 'lucide-angular';
 import { HlmButtonDirective } from '../ui/ui-button-helm/src';
 import { SidebarService } from './sidebar-service';
 import { ToggleSidebarDirective } from './toggle-sidebar-directive';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, HlmButtonDirective, ToggleSidebarDirective],
+  imports: [
+    RouterOutlet, 
+    HlmButtonDirective, 
+    ToggleSidebarDirective,
+    LucideAngularModule
+  ],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
 export class Layout {
+  PanelLeftIcon = PanelLeft
   open!: Signal<boolean>
   
   constructor(
