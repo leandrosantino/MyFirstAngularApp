fa45574f1e9de8e4757ff6cac17fe6ab5900c908
leandrosantino <leandrosantino2013@gmail.com>
2025-06-18 22:27:29 -0300

    update

--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -1,17 +1,19 @@
 import { Component, Signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LucideAngularModule, PanelLeft } from 'lucide-angular';
+import { ChartColumnBig, ListChecks, LucideAngularModule, PanelLeft, ScanBarcode } from 'lucide-angular';
 import { HlmButtonDirective } from '../ui/ui-button-helm/src';
+import { HlmToggleDirective } from '../ui/ui-toggle-helm/src';
 import { SidebarService } from './sidebar-service';
 import { ToggleSidebarDirective } from './toggle-sidebar-directive';
 
 @Component({
   selector: 'app-layout',
   imports: [
-    RouterOutlet, 
-    HlmButtonDirective, 
+    RouterOutlet,
+    HlmButtonDirective,
     ToggleSidebarDirective,
-    LucideAngularModule
+    LucideAngularModule,
+    HlmToggleDirective
   ],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
@@ -19,10 +21,20 @@ import { ToggleSidebarDirective } from './toggle-sidebar-directive';
 export class Layout {
   PanelLeftIcon = PanelLeft
   open!: Signal<boolean>
-  
+
+  menuButtons = [
+    { title: "Dashboard", url: "/dash", icon: ChartColumnBig, requiredRoles: ['ADMIN'] },
+    { title: "Ordens de Serviço", url: "/service-orders", icon: ListChecks, requiredRoles: ['ADMIN'] },
+    { title: "Produtos", url: "/", icon: ScanBarcode, requiredRoles: ['ADMIN'] },
+  ]
+
+  a(e: any) {
+    console.log(e)
+  }
+
   constructor(
     private readonly sidebarSerice: SidebarService
-  ){
+  ) {
     this.open = this.sidebarSerice.open
   }
 
