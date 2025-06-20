33ba661917da07ac8eabaf33b6eb4cf05e02a6be
leandrosantino <leandrosantino2013@gmail.com>
2025-06-20 20:38:29 -0300

    update

--- src/app/components/layout/layout.ts
+++ src/app/components/layout/layout.ts
@@ -1,8 +1,14 @@
 import { AuthService, UserProfile } from '@/app/service/auth';
-import { Component, Signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, effect, OnDestroy, OnInit, Signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucideChartColumnBig, lucideListChecks, lucidePanelLeft, lucideScanBarcode } from '@ng-icons/lucide';
+import { BrnSheetContentDirective, BrnSheetTriggerDirective } from '@spartan-ng/brain/sheet';
+import {
+  HlmSheetComponent,
+  HlmSheetContentComponent
+} from '@spartan-ng/helm/sheet';
 import { LucideAngularModule } from 'lucide-angular';
 import { ProfileMenu } from '../profile-menu/profile-menu';
 import { HlmButtonDirective } from '../ui/ui-button-helm/src';
@@ -14,15 +20,21 @@ import { ToggleSidebarDirective } from './toggle-sidebar-directive';
 @Component({
   selector: 'app-layout',
   imports: [
+    CommonModule,
     RouterOutlet,
     HlmButtonDirective,
     ToggleSidebarDirective,
     LucideAngularModule,
     HlmToggleDirective,
-    
-    HlmIconDirective, 
+
+    HlmIconDirective,
     NgIcon,
     ProfileMenu,
+
+    BrnSheetTriggerDirective,
+    BrnSheetContentDirective,
+    HlmSheetComponent,
+    HlmSheetContentComponent
   ],
   providers: [
     provideIcons({
@@ -35,8 +47,8 @@ import { ToggleSidebarDirective } from './toggle-sidebar-directive';
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
-export class Layout {
-  
+export class Layout implements OnInit, OnDestroy {
+
   menuButtons = [
     { isActive: false, title: "Dashboard", url: "/", icon: 'lucideChartColumnBig', requiredRoles: ['ADMIN'] },
     { isActive: false, title: "Ordens de Serviço", url: "/service-orders", icon: 'lucideListChecks', requiredRoles: ['ADMIN'] },
@@ -44,31 +56,54 @@ export class Layout {
   ]
   userProfile?: UserProfile
   open!: Signal<boolean>
-  
+  isMobile!: Signal<boolean>
+
+
+  sheetCtx: any
+
   constructor(
     private readonly sidebarSerice: SidebarService,
     private readonly authService: AuthService,
     private readonly router: Router
   ) {
     this.open = this.sidebarSerice.open
+    this.isMobile = this.sidebarSerice.isMobile
     this.userProfile = this.authService.getUserProfile()
     this.setMenuButtonActive()
+
+    effect(() => {
+      console.log('isMobile:', this.isMobile());
+    })
+
   }
 
-  getPageTitle(){
+
+  private pageSizeListennerUnsubscriber!: Function
+
+  ngOnInit(): void {
+    this.pageSizeListennerUnsubscriber = this.sidebarSerice.startPageSizeListenner()
+  }
+
+  ngOnDestroy(): void {
+    this.pageSizeListennerUnsubscriber()
+  }
+
+
+  getPageTitle() {
     return this.menuButtons.find(item => item.isActive)?.title
   }
-  
-  async navigate(url: string){
+
+  async navigate(url: string) {
     await this.router.navigate([url])
     this.setMenuButtonActive()
   }
 
-  setMenuButtonActive(){
+  setMenuButtonActive() {
     this.menuButtons = this.menuButtons.map(item => {
       item.isActive = item.url == this.router.url
       return item
     })
   }
+
 
 }
