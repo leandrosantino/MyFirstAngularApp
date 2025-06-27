6cdd6149009a1a14bb6880f2c0fa3e406b2e3661
leandrosantino <leandrosantino2013@gmail.com>
2025-06-26 21:18:15 -0300

    feat: Add pipe to format date of service orders card

--- src/app/components/order-card/order-card.ts
+++ src/app/components/order-card/order-card.ts
@@ -1,11 +1,12 @@
 import { ServiceOrder } from '@/app/services/orders';
+import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { twMerge } from 'tailwind-merge';
 import { HlmBadgeDirective } from '../ui/ui-badge-helm/src';
 
 @Component({
   selector: 'app-order-card',
-  imports: [HlmBadgeDirective],
+  imports: [HlmBadgeDirective, DatePipe],
   templateUrl: './order-card.html',
   styles: [':host { display: contents }']
 })
